dee9b0bfd198b4c563f00cfa984c4b6644c4c952
Roberto Carlos <25rcsanchez1206@gmail.com>
2022-11-29 15:54:53 -0600

    Programación de la Interfaz Nuevo Producto: Parra - 15:54 pm 29/11/2022

--- src/Interfaces/JINuevoProducto.java
+++ src/Interfaces/JINuevoProducto.java
@@ -1,16 +1,71 @@
 package Interfaces;
 
+import java.awt.Color;
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class JINuevoProducto extends javax.swing.JInternalFrame {
+
+    int idUnidadMedida;
+    int idProveedor;
+    int idCategoria;
 
     public JINuevoProducto() {
         initComponents();
         this.setSize(794, 548);
         this.setTitle("Nuevo Producto");
-        
+
+        try {
+            Connection cn = Conexion.conectar();
+            PreparedStatement pst = cn.prepareStatement(
+                    "select NombreUDM from unidaddemedida");
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                cmbUDM.addItem(rs.getString("NombreUDM"));
+            }
+            cn.close();
+
+        } catch (SQLException e) {
+            System.err.println("Error al llenar los estados. " + e);
+            JOptionPane.showMessageDialog(null, "Error al mostrar información, Contacte al Administrador");
+        }
+
+        try {
+            Connection cn = Conexion.conectar();
+            PreparedStatement pst = cn.prepareStatement(
+                    "select NombreProveedor from proveedor");
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                cmbProveedores.addItem(rs.getString("NombreProveedor"));
+            }
+            cn.close();
+
+        } catch (SQLException e) {
+            System.err.println("Error al llenar los estados. " + e);
+            JOptionPane.showMessageDialog(null, "Error al mostrar información, Contacte al Administrador");
+        }
+
+        try {
+            Connection cn = Conexion.conectar();
+            PreparedStatement pst = cn.prepareStatement(
+                    "select NombreCategoria from categorias");
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                cmbCategorias.addItem(rs.getString("NombreCategoria"));
+            }
+            cn.close();
+
+        } catch (SQLException e) {
+            System.err.println("Error al llenar los estados. " + e);
+            JOptionPane.showMessageDialog(null, "Error al mostrar información, Contacte al Administrador");
+        }
 
     }
-    
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -35,7 +90,7 @@ public class JINuevoProducto extends javax.swing.JInternalFrame {
         txtPrecioV = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         btnBuscar = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        btnAgregarPro = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setClosable(true);
@@ -50,123 +105,124 @@ public class JINuevoProducto extends javax.swing.JInternalFrame {
         lblCodigo.setText("Código de Barras:");
         jPanel1.add(lblCodigo, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 50, -1, -1));
 
-        txtCodigo.setBackground(new java.awt.Color(0, 153, 102));
         txtCodigo.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
-        txtCodigo.setForeground(new java.awt.Color(255, 255, 255));
+        txtCodigo.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         txtCodigo.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         txtCodigo.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 txtCodigoKeyTyped(evt);
             }
         });
-        jPanel1.add(txtCodigo, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 47, 155, -1));
+        jPanel1.add(txtCodigo, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 47, 200, -1));
 
         lblProducto.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         lblProducto.setForeground(new java.awt.Color(255, 255, 255));
         lblProducto.setText("Nombre del Producto:");
         jPanel1.add(lblProducto, new org.netbeans.lib.awtextra.AbsoluteConstraints(22, 113, -1, -1));
 
-        txtProducto.setBackground(new java.awt.Color(0, 153, 102));
         txtProducto.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
-        txtProducto.setForeground(new java.awt.Color(255, 255, 255));
+        txtProducto.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         txtProducto.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         txtProducto.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 txtProductoKeyTyped(evt);
             }
         });
-        jPanel1.add(txtProducto, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 110, 233, -1));
+        jPanel1.add(txtProducto, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 110, 200, -1));
 
         lblExistencia.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         lblExistencia.setForeground(new java.awt.Color(255, 255, 255));
         lblExistencia.setText("Existencia:");
         jPanel1.add(lblExistencia, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 173, -1, -1));
 
-        txtExistencia.setBackground(new java.awt.Color(0, 153, 102));
         txtExistencia.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
-        txtExistencia.setForeground(new java.awt.Color(255, 255, 255));
+        txtExistencia.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         txtExistencia.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jPanel1.add(txtExistencia, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 170, 155, -1));
+        txtExistencia.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtExistenciaKeyTyped(evt);
+            }
+        });
+        jPanel1.add(txtExistencia, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 170, 200, -1));
 
         lblExistencia1.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         lblExistencia1.setForeground(new java.awt.Color(255, 255, 255));
         lblExistencia1.setText("Unidad de Medida:");
         jPanel1.add(lblExistencia1, new org.netbeans.lib.awtextra.AbsoluteConstraints(46, 233, -1, -1));
 
-        cmbUDM.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
-        jPanel1.add(cmbUDM, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 230, 160, -1));
+        cmbUDM.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
+        jPanel1.add(cmbUDM, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 230, 200, -1));
 
         lblProveedor.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         lblProveedor.setForeground(new java.awt.Color(255, 255, 255));
         lblProveedor.setText("Proveedores:");
         jPanel1.add(lblProveedor, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 293, -1, -1));
 
-        cmbProveedores.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
-        jPanel1.add(cmbProveedores, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 290, 160, -1));
+        cmbProveedores.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
+        jPanel1.add(cmbProveedores, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 290, 200, -1));
 
         lblCategorias.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         lblCategorias.setForeground(new java.awt.Color(255, 255, 255));
         lblCategorias.setText("Categorias:");
         jPanel1.add(lblCategorias, new org.netbeans.lib.awtextra.AbsoluteConstraints(95, 353, -1, -1));
 
-        cmbCategorias.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
-        jPanel1.add(cmbCategorias, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 350, 160, -1));
+        cmbCategorias.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
+        jPanel1.add(cmbCategorias, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 350, 200, -1));
 
         lblPrecioCompra.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         lblPrecioCompra.setForeground(new java.awt.Color(255, 255, 255));
         lblPrecioCompra.setText("Precio Compra:");
         jPanel1.add(lblPrecioCompra, new org.netbeans.lib.awtextra.AbsoluteConstraints(68, 413, -1, -1));
 
-        txtPrecioC.setBackground(new java.awt.Color(0, 153, 102));
         txtPrecioC.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
-        txtPrecioC.setForeground(new java.awt.Color(255, 255, 255));
+        txtPrecioC.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         txtPrecioC.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         txtPrecioC.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 txtPrecioCKeyTyped(evt);
             }
         });
-        jPanel1.add(txtPrecioC, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 410, 155, -1));
+        jPanel1.add(txtPrecioC, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 410, 200, -1));
 
         lblPrecioVenta.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
         lblPrecioVenta.setForeground(new java.awt.Color(255, 255, 255));
         lblPrecioVenta.setText("Precio Venta:");
         jPanel1.add(lblPrecioVenta, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 473, -1, -1));
 
-        txtPrecioV.setBackground(new java.awt.Color(0, 153, 102));
         txtPrecioV.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
-        txtPrecioV.setForeground(new java.awt.Color(255, 255, 255));
+        txtPrecioV.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         txtPrecioV.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         txtPrecioV.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 txtPrecioVKeyTyped(evt);
             }
         });
-        jPanel1.add(txtPrecioV, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 470, 155, -1));
+        jPanel1.add(txtPrecioV, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 470, 200, -1));
 
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 70, 160, 140));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 70, 160, 140));
 
         btnBuscar.setFont(new java.awt.Font("Roboto Light", 0, 11)); // NOI18N
-        btnBuscar.setText("Buscar Imagen");
+        btnBuscar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Buscar Archivo.png"))); // NOI18N
+        btnBuscar.setBorder(null);
         btnBuscar.setPreferredSize(new java.awt.Dimension(50, 23));
         btnBuscar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnBuscarActionPerformed(evt);
             }
         });
-        jPanel1.add(btnBuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 90, 110, 90));
+        jPanel1.add(btnBuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 90, 100, 100));
 
-        jButton1.setFont(new java.awt.Font("Roboto Light", 0, 11)); // NOI18N
-        jButton1.setText("Agregar Producto");
-        jButton1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnAgregarPro.setFont(new java.awt.Font("Roboto Light", 0, 11)); // NOI18N
+        btnAgregarPro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/agregar.png"))); // NOI18N
+        btnAgregarPro.setBorder(null);
+        btnAgregarPro.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnAgregarProActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 350, 130, 100));
+        jPanel1.add(btnAgregarPro, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 350, 100, 100));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -184,51 +240,137 @@ public class JINuevoProducto extends javax.swing.JInternalFrame {
 
     private void txtCodigoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtCodigoKeyTyped
 
-        if (txtCodigo.getText().length() > 12) {
+        if (txtCodigo.getText().length() >= 12) {
             evt.consume();
         }
 
     }//GEN-LAST:event_txtCodigoKeyTyped
 
     private void txtProductoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtProductoKeyTyped
-       
-        if (txtProducto.getText().length() > 45) {
+
+        if (txtProducto.getText().length() >= 45) {
             evt.consume();
         }
-        
+
     }//GEN-LAST:event_txtProductoKeyTyped
 
     private void txtPrecioCKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPrecioCKeyTyped
-        
-         if (txtPrecioC.getText().length() > 5) {
+
+        if (txtPrecioC.getText().length() >= 5) {
             evt.consume();
         }
-        
+
     }//GEN-LAST:event_txtPrecioCKeyTyped
 
     private void txtPrecioVKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPrecioVKeyTyped
-      
-        if (txtPrecioV.getText().length() > 5) {
+
+        if (txtPrecioV.getText().length() >= 5) {
+            evt.consume();
+        }
+
+    }//GEN-LAST:event_txtPrecioVKeyTyped
+
+    private void txtExistenciaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtExistenciaKeyTyped
+       
+        if (txtExistencia.getText().length() >= 5) {
             evt.consume();
         }
         
-    }//GEN-LAST:event_txtPrecioVKeyTyped
+    }//GEN-LAST:event_txtExistenciaKeyTyped
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
+
+        
         
     }//GEN-LAST:event_btnBuscarActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void btnAgregarProActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarProActionPerformed
+
+        int validacion = 0;
+    
+    String CB, NP, Ex, PC, PV;
+    
+    CB = txtCodigo.getText().trim();
+    NP = txtProducto.getText().trim();
+    Ex = txtExistencia.getText().trim();
+    PC = txtPrecioC.getText().trim();
+    PV = txtPrecioV.getText().trim();
+    
+    idUnidadMedida = cmbUDM.getSelectedIndex() + 1;
+    idProveedor = cmbProveedores.getSelectedIndex() +1;
+    idCategoria = cmbCategorias.getSelectedIndex() +1;
+    
+    if (CB.equals("")) {
+            txtCodigo.setBackground(Color.red);
+            validacion++;
+        }
+        if (NP.equals("")) {
+            txtProducto.setBackground(Color.red);
+            validacion++;
+        }
+        if (Ex.equals("")) {
+            txtExistencia.setBackground(Color.red);
+            validacion++;
+        }
+        if (PC.equals("")) {
+            txtPrecioC.setBackground(Color.red);
+            validacion++;
+        }
+        if (PV.equals("")) {
+            txtPrecioV.setBackground(Color.red);
+            validacion++;
+        }
+        
+        if (validacion == 0) {
+            Connection cn = Conexion.conectar();
+            PreparedStatement pst;
+            try {
+                pst = cn.prepareStatement(
+                        "insert into productos values(?,?,?,?,?,?,?,?,?,?,?)");
+                pst.setString(1, "0");
+                pst.setString(2, CB);
+                pst.setString(3, NP);
+                pst.setString(4, Ex);
+                pst.setInt(5, idUnidadMedida);
+                pst.setInt(6, idProveedor);
+                pst.setInt(7, idCategoria);
+                pst.setString(8, PC);
+                pst.setString(9, PV);
+                pst.setString(10, "1");
+                pst.setString(11, "1");
+                
+                pst.executeUpdate();
+                cn.close();
+
+                txtCodigo.setText("");
+                txtProducto.setText("");
+                txtExistencia.setText("");
+                cmbUDM.setSelectedIndex(0);
+                cmbProveedores.setSelectedIndex(0);
+                cmbCategorias.setSelectedIndex(0);
+                txtPrecioC.setText("");
+                txtPrecioV.setText("");
+                
+
+                JOptionPane.showMessageDialog(null, "Registro de Producto Exitoso");
+            } catch (SQLException e) {
+                System.err.println("Error en Registrar Producto." + e);
+                JOptionPane.showMessageDialog(null, "¡¡ERROR al registrar!!, contacte al administrador.");
+            }
+            
+        } else {
+            JOptionPane.showMessageDialog(null, "Debes llenar todos los campos.");
+        }
+
+    }//GEN-LAST:event_btnAgregarProActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAgregarPro;
     private javax.swing.JButton btnBuscar;
     private javax.swing.JComboBox<String> cmbCategorias;
     private javax.swing.JComboBox<String> cmbProveedores;
     private javax.swing.JComboBox<String> cmbUDM;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblCategorias;
